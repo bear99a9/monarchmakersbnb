f75ad624c855c052f7a7ca1a04cf910c5539ae39
Anna Cavalla <anna.cavalla@gmail.com>
2021-02-03 15:28:54 +0000

    Refactored

--- spec/units/booking_spec.rb
+++ spec/units/booking_spec.rb
@@ -1,7 +1,8 @@
 describe Booking do
 
-  let(:user) { create_user }
-  let(:listing) { create_user_and_listing }
+  let(:user) { create_ollie }
+  let(:user_2) { create_anna }
+  let(:listing) { create_listing(user_id: user.id) }
 
   # let(:results) { persisted_data(id: new_user.id, table: 'booking') }
   subject(:booking) { described_class.create(visitor_id: user.id, listing_id: listing.id) }
@@ -15,28 +16,31 @@ describe Booking do
 
   describe '.all' do
     it 'lists all bookings' do
+      booking
       new_booking = Booking.create(visitor_id: user.id, listing_id: listing.id)
-      Booking.create(visitor_id: user.id, listing_id: listing.id)
 
       bookings = Booking.all
 
       expect(bookings.length).to eq 2
       expect(bookings).to all(be_an_instance_of(Booking))
-      expect(bookings.last.id).to eq new_booking.id
-      expect(bookings.last.visitor_id).to eq new_booking.visitor_id
-      expect(bookings.last.listing_id).to eq new_booking.listing_id
+      expect(bookings.first.id).to eq new_booking.id
+      expect(bookings.first.visitor_id).to eq new_booking.visitor_id
+      expect(bookings.first.listing_id).to eq new_booking.listing_id
     end
   end
 
   describe '.find_all_by_user' do
     it 'displays all bookings by current user' do
-      Booking.create(visitor_id: user.id, listing_id: listing.id)
       listing_2 = Listing.create(name: 'My other place', description: '2 bed', price_per_night: 200, user_id: user.id)
-      Booking.create(visitor_id: user.id, listing_id: listing_2.id)
+
+      booking
+      booking_2 = Booking.create(visitor_id: user.id, listing_id: listing_2.id)
+      booking_3 = Booking.create(visitor_id: user_2.id, listing_id: listing.id)
 
       bookings = Booking.find_all_by_user(visitor_id: user.id)
 
       expect(bookings.length).to eq 2
+      expect(bookings).not_to include booking_3
       expect(bookings).to all(be_an_instance_of(Booking))
       expect(bookings.first.visitor_id).to eq user.id
       expect(bookings.last.visitor_id).to eq user.id
--- spec/units/test_helpers.rb
+++ spec/units/test_helpers.rb
@@ -1,8 +1,11 @@
-def create_user
+def create_ollie
   User.create(name: "Ollie", email: "ollie@gmail.com", username: "Ollie", password: "password123")
 end
 
-def create_user_and_listing 
-  user = User.create(name: "Anna", email: "anna@gmail.com", username: "acav", password: "password123")
-  Listing.create(name: 'My other place', description: '2 bed', price_per_night: 200, user_id: user.id)
+def create_anna
+  User.create(name: "Anna", email: "anna@gmail.com", username: "acav", password: "password123")
+end 
+
+def create_listing(user_id:)
+  Listing.create(name: 'My other place', description: '2 bed', price_per_night: 200, user_id: user_id)
 end
