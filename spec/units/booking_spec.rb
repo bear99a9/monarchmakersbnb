f740a3902f6ed80e133c0834208867af7c8e4ca3
bateso88 <bateso@hotmail.co.uk>
2021-02-03 12:41:29 +0000

    have written a unit test for Booking.all

--- spec/units/booking_spec.rb
+++ spec/units/booking_spec.rb
@@ -1,13 +1,29 @@
 describe Booking do
+
+  let(:user) { create_user }
+  let(:listing) { create_user_and_listing }
+  # let(:results) { persisted_data(id: new_user.id, table: 'booking') }
+  subject(:booking) { described_class.create(visitor_id: user.id, listing_id: listing.id) }
   describe '.create' do 
     it 'returns a booking' do 
-      user = create_user
-      listing = create_listing
-      booking = Booking.create(visitor_id: user.id, listing_id: listing.id, status: 'pending')
-
       expect(booking.visitor_id).to eq user.id
       expect(booking.listing_id).to eq listing.id
       expect(booking.status).to eq 'pending'
+    end
+  end
+
+  describe '.all' do
+    it 'lists all bookings' do 
+      new_booking = Booking.create(visitor_id: user.id, listing_id: listing.id)
+      Booking.create(visitor_id: user.id, listing_id: listing.id)
+
+      bookings = Booking.all
+
+      expect(bookings.length).to eq 2
+      expect(bookings).to all(be_an_instance_of(Booking))
+      expect(bookings.last.id).to eq new_booking.id
+      expect(bookings.last.visitor_id).to eq new_booking.visitor_id
+      expect(bookings.last.listing_id).to eq new_booking.listing_id
     end
   end
 end
--- spec/units/test_helpers.rb
+++ spec/units/test_helpers.rb
@@ -2,7 +2,7 @@ def create_user
   User.create(name: "Ollie", email: "ollie@gmail.com", username: "Ollie", password: "password123")
 end
 
-def create_listing 
+def create_user_and_listing 
   user = User.create(name: "Anna", email: "anna@gmail.com", username: "acav", password: "password123")
   Listing.create(name: 'My other place', description: '2 bed', price_per_night: 200, user_id: user.id)
 end
