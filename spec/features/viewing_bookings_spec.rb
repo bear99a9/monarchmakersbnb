307bb24002500746e0c88404c67598506620a402
CodeRed30 <katrinaharradine@gmail.com>
2021-02-05 11:01:21 +0000

    CSS structure

---
+++ spec/features/viewing_bookings_spec.rb
@@ -0,0 +1,25 @@
+feature "viewing bookings" do
+  # let(:anna) { create_anna }
+  # let(:listing) { create_listing(user_id: anna.id) }
+
+  # before do
+  #   anna
+  #   listing
+  #
+  #   click_link(listing.name)
+  # end
+
+  scenario 'view bookings from listings' do
+    create_user_and_sign_in
+    click_button('My Bookings')
+    expect(current_path).to match /\/users\/.+\/bookings/
+    expect(page).to have_content("Here are your bookings")
+  end
+
+  scenario 'return to listings from my bookings' do
+    create_user_and_sign_in
+    click_button('My Bookings')
+    click_button('Back to listings')
+    expect(current_path).to eq('/listings')
+  end
+end
